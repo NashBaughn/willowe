b74845fcf947f7f55738657a3986bd40f501aa73
seangithub1 <seangaudefroy@gmail.com>
2018-02-23 15:34:03 -0800

    Update index.js
    Legal disclaimer to display on legal page

--- NashBP/src/screens/Legal/index.js
+++ NashBP/src/screens/Legal/index.js
@@ -26,7 +26,7 @@ class LegalScreen extends React.Component<Props, State> {
 				</Header>
 
 				<Content padder>
-					<Text>{param !== undefined ? param.name.item : "Create Something Awesome . . ."}</Text>
+					<Text>{param !== undefined ? param.name.item : "Disclaimer: Willowe is not a law firm, we are not responsible for any damages of use or misuse of this software.  Willowe is meant to provide a statement of final wishes, we do not make any claims of legality of service.  We recommend you see an attorney to create a legal will, the law is highly complex and professionals should be sought out to make decisions on the law.  Willowe is not intended for public or commercial use and should not be released, it does not provide any guarantee of services.  Willowe is part of an educational exercise and should not be mistaken for a marketable product.  All lawful responsibilities lay with the user of this software."}</Text>
 				</Content>
 			</Container>
 		);
