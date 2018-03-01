4926d87984270e01ba34c97fdae2003773b6ad92
NashBaguhn <nbaughn@ucsc.edu>
2018-03-01 14:57:34 -0800

    camera component updated

--- FinalApp/src/components/CameraDetail.js
+++ FinalApp/src/components/CameraDetail.js
@@ -5,10 +5,114 @@ import { FileSystem, FaceDetector } from 'expo';
 
 const pictureSize = 150;
 
+
 export interface State {}
 export default class CameraDetails extends React.Component<Props, State> {
+  state = {
+    faces: {},
+    images: {},
+    photos: [],
+  };
+  
+  _mounted = false;
+  
+  componentDidMount() {
+    console.log("did mount")
+    this._mounted = true;
+    FileSystem.readDirectoryAsync(FileSystem.documentDirectory + 'photos').then(photos => {
+      if (this._mounted) {
+        this.setState(
+          {
+            photos,
+          },
+          this.detectFaces
+        );
+      }
+    });
+  }
+  componentWillUnmount() {
+    this._mounted = false;
+  }
+
+  getImageDimensions = ({ width, height }) => {
+    if (width > height) {
+      const scaledHeight = pictureSize * height / width;
+      return {
+        width: pictureSize,
+        height: scaledHeight,
+
+        scaleX: pictureSize / width,
+        scaleY: scaledHeight / height,
+
+        offsetX: 0,
+        offsetY: (pictureSize - scaledHeight) / 2,
+      };
+    } else {
+      const scaledWidth = pictureSize * width / height;
+      return {
+        width: scaledWidth,
+        height: pictureSize,
+
+        scaleX: scaledWidth / width,
+        scaleY: pictureSize / height,
+
+        offsetX: (pictureSize - scaledWidth) / 2,
+        offsetY: 0,
+      };
+    }
+  };
+
+  detectFaces = () => this.state.photos.forEach(this.detectFace);
+
+  detectFace = photoUri =>
+    FaceDetector.detectFacesAsync(`${FileSystem.documentDirectory}photos/${photoUri}`, {
+      detectLandmarks: FaceDetector.Constants.Landmarks.none,
+      runClassifications: FaceDetector.Constants.Classifications.all,
+    })
+      .then(this.facesDetected)
+      .catch(this.handleFaceDetectionError);
+
+  facesDetected = ({ image, faces }) => {
+    if (!this._mounted) return;
+    this.setState({
+      faces: { ...this.state.faces, [image.uri]: faces },
+      images: { ...this.state.images, [image.uri]: image },
+    });
+  }
+
+  handleFaceDetectionError = error => console.warn(error);
+
+  renderFaces = photoUri =>
+    this.state.images[photoUri] &&
+    this.state.faces[photoUri] &&
+    this.state.faces[photoUri].map(this.renderFace(this.state.images[photoUri]));
+
+  renderFace = image => (face, index) => {
+    const { scaleX, scaleY, offsetX, offsetY } = this.getImageDimensions(image);
+    const layout = {
+      top: offsetY + face.bounds.origin.y * scaleY,
+      left: offsetX + face.bounds.origin.x * scaleX,
+      width: face.bounds.size.width * scaleX,
+      height: face.bounds.size.height * scaleY,
+    };
+
+    return (
+      <View
+        key={index}
+        style={[styles.face, layout]}
+        transform={[
+          { perspective: 600 },
+          { rotateZ: `${(face.rollAngle || 0).toFixed(0)}deg` },
+          { rotateY: `${(face.yawAngle || 0).toFixed(0)}deg` },
+        ]}>
+      </View>
+    );
+  };
+
   render() {
     const param = this.props.navigation.state.params;
+    console.log(this.state.photos[0]);
+    const photoUri = this.state.photos[0];
     return (
       <Container style={styles.container}>
         <Header>
@@ -27,7 +131,42 @@ export default class CameraDetails extends React.Component<Props, State> {
         </Header>
 
         <Content padder>
-          <Text>{param !== undefined ? param.name.item : "Create Something Awesome . . ."}</Text>
+          <View>           
+           {/* <ScrollView contentComponentStyle={{ flex: 1 }}>
+              <View style={styles.pictures}>
+                {this.state.photos.map(photoUri => (
+                  <View style={styles.pictureWrapper} key={photoUri}>
+                    <Image
+                      key={photoUri}
+                      style={styles.picture}
+                      source={{
+                        uri: `${FileSystem.documentDirectory}photos/${photoUri}`,
+                      }}
+                    />
+                    <View style={styles.facesContainer}>
+                      {this.renderFaces(`${FileSystem.documentDirectory}photos/${photoUri}`)}
+                    </View>
+                  </View>
+                ))}
+              </View>
+            </ScrollView>*/}
+
+            <View style={styles.pictures}>
+              <View style={styles.pictureWrapper} >
+                <Image
+                  key={photoUri}
+                  style={styles.picture}
+                  source={{
+                    uri: `${FileSystem.documentDirectory}photos/${photoUri}`,
+                  }}
+                />
+                <View style={styles.facesContainer}>
+                  {this.renderFaces(`${FileSystem.documentDirectory}photos/${photoUri}`)}
+                </View>
+              </View>
+            </View>
+
+          </View>          
         </Content>
       </Container>
     );
@@ -37,5 +176,49 @@ export default class CameraDetails extends React.Component<Props, State> {
 const styles: any = StyleSheet.create({
   container: {
     backgroundColor: "#FBFAFA",
+  },
+  pictures: {
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  picture: {
+    width : 300,
+    height: 300,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+    resizeMode: 'contain',
+  },
+  pictureWrapper: {
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  facesContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    left: 0,
+    top: 0,
+  },
+  face: {
+    borderWidth: 2,
+    borderRadius: 2,
+    position: 'absolute',
+    borderColor: '#FFD700',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  faceText: {
+    color: '#FFD700',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 2,
+    fontSize: 20,
+    backgroundColor: 'transparent',
   },
 });
